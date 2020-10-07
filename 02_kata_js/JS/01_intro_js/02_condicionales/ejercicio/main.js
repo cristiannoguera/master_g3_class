17edb30516f0fb8096e16568d5158ac98c7f68b3
cristiannoguera <cnoguera18@gmail.com>
2020-10-07 11:39:54 -0500

    se sube el ejecicio 2

--- 02_kata_js/JS/01_intro_js/02_condicionales/ejercicio/main.js
+++ 02_kata_js/JS/01_intro_js/02_condicionales/ejercicio/main.js
@@ -14,23 +14,6 @@ if (isNaN(numero)) {
     }
 }*/
 
-//Validar si se puede conducir
-
-var edadPregunta = prompt('Que edad tienes?');
-
-var edadNumero = Number(edadPregunta);
-
-if (edadNumero >= 18 && edadNumero <= 80) {
-    console.log('Puedes conducir')
-}else if (edadNumero === 16 || edadNumero ==17) {
-    console.log(' Puedes sacar tu permiso para conducir')
-}else if (edadNumero <=15 || edadNumero >80) {
-    console.warn('No puedes conducir por tu seguridad')
-}else {
-    console.error('No es una edad');
-}
-
-
 //-------- Challenge 2 --------
 // Juego de piedra papel o tijera :scissors: ⛰ :page_with_curl:
 // Preguntar a 2 usuarios que opcion eleguir
