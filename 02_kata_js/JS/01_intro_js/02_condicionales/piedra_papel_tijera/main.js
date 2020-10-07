de3aae2b7efadb7055a8d29cf07b54cee34079dc
cristiannoguera <cnoguera18@gmail.com>
2020-10-07 16:58:40 -0500

    se sube el ejecicio casa

---
+++ 02_kata_js/JS/01_intro_js/02_condicionales/piedra_papel_tijera/main.js
@@ -0,0 +1,15 @@
+
+var piedra = 1
+var papel = 2
+var tijera = 3
+
+var player1 = prompt('piedra, papel o tijera');
+var player2 = prompt('piedra, papel o tijera');
+
+if (player1 === piedra, player2 === papel) {
+    console.log ('El ganador es el player2')
+    if (player1 === papel, player2 === tijera) {
+        console.log ('El ganador es el player2')
+        if (player1 === tijera, player2 === piedra)
+    }
+}
