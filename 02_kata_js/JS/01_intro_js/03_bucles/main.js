50f01b166e010fe2be9232345f32eeddd2cf688b
cristiannoguera <cnoguera18@gmail.com>
2020-10-07 21:54:45 -0500

    se agrega el challenge de bucles

---
+++ 02_kata_js/JS/01_intro_js/03_bucles/main.js
@@ -0,0 +1,15 @@
+
+/*
+for (var i = 10; i <= 1000; i+=250) {
+    console.log(i);
+}
+*/
+
+for (var i = -500; i=0; i-=-10) {
+    console.log(i);
+}
+
+/*for (i = 0; i<=1000; i +=50) {
+    console.log(i)
+}*/
+
