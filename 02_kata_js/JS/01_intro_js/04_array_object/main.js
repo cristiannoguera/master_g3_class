34801e58dae3876f869bd3ea21372d4d73ff2ca8
cristiannoguera <cnoguera18@gmail.com>
2020-10-12 20:07:07 -0500

    Cargando kata04-js

---
+++ 02_kata_js/JS/01_intro_js/04_array_object/main.js
@@ -0,0 +1,76 @@
+
+
+// array o arreglos
+
+//posicion    0         1        2     
+var color = ['azul', 'verde', 'naranja']
+
+console.log(color)
+console.log(color[1])
+
+//------Metodos------//
+// .push()
+// empuje un nuevo valor a la ultima posición
+
+color.push('Negro');
+color.push('Rosa');
+console.log(color);
+
+// .pop()
+// Eliminar el ultimo valor
+
+color.pop()
+console.log(color)
+
+//----------------->Objetos o objects<------------
+
+// var Objeto = {}
+
+var persona = {
+    nombre: 'Cristian',
+    edad: 29,
+    cel: '+57 3144449328',
+    musica: {
+        banda: 'Banda MS',
+        kpop: 'k-pop',
+        rock: 'kiss',
+        trap: {
+            duki: 'Modo diablo',
+        },
+        peliculas: ['RF1', 'RF2', 'RF3',]
+};
+
+console.log(persona.edad);
+console.log(persona.nombre);
+console.log(persona.musica.trap.duki);
+console.log(persona.peliculas[2])
+
+// array with objects
+var animales = [
+    {
+        nombre: 'vaca',
+        comida: true
+    },
+    {
+        nombre: 'Aguila',
+        comida: false
+    },
+    {
+        nombre: 'ballena',
+        comida: false
+    },
+    {
+        nombre: 'Murcielago',
+        comida: false
+    },
+];
+
+console.log(animales[3].nombre)
+
+
+
+
+
+
+
+
