70406b27093bb1258890e7d6a549ec0f43ee7817
cristiannoguera <cristian.noguera@clouxter.com>
2020-11-11 20:57:03 -0500

    challenge

--- 03_kata_utilities/03_POO/main.js
+++ 03_kata_utilities/03_POO/main.js
@@ -1,10 +1,34 @@
 class Animal {
     // atributo
-    constructor(nombre, edad){
+    constructor(nombre, edad, color, raza, tipo){
         this.nombre = nombre
-        this.edad = edad 
+        this.edad = edad
+        this.color = color
+        this.raza = raza
+        this.tipo = tipo 
     }
 
-    //
-
+    //Metodo
+    dormir(){
+        return `Mi mascota llamado ${this.nombre} duerme porque ya tiene ${this.edad}`
+    }
+    comer(){
+        return `${this.nombre} come mucha galleta`
+    }
 }
+    const miMascota = new Animal('Paco', 2, 'cafe', 'cruza', 'perro')
+    const miMascota2 = new Animal('Rocko', 5)
+
+    console.log(miMascota)
+    console.log(miMascota.dormir())
+    console.log(miMascota2)
+    console.log(miMascota2.dormir())
+    console.log(miMascota.comer())
+
+
+//Challege
+
+    // Crear una clase llamada automovil, y van a tener 3 propiedades. color, marca, deportivo=bolean
+        // 2 metodos (Lo que quieran)
+        
+        // crear 3 carros distintos
