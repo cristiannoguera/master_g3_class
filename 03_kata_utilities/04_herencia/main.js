0518d36fe1db073b6c8870752e7ea323ecee0bbf
cristiannoguera <cristian.noguera@clouxter.com>
2020-11-11 22:38:14 -0500

    Challenge2

--- 03_kata_utilities/04_herencia/main.js
+++ 03_kata_utilities/04_herencia/main.js
@@ -15,11 +15,59 @@ class Perrito extends Mascota {
     ladrar(){
         return 'woooooowf'
     }
-};
+}
+class Gato extends Mascota {
+    constructor (nombre, patas, raza){
+        super(nombre, patas)
+        this.raza = raza
+    }
+    maulla(){
+        return 'miauuuuu'
+    }
+}
 
+class Hamters extends Mascota {
+    constructor (nombre, patas, tipo){
+        super(nombre, patas)
+        this.tipo = tipo
+    }
+    jugar(){
+        return 'yuujuuuu'
+    }
+}
+class Gallina extends Mascota {
+    constructor (nombre, patas, alas){
+        super(nombre, patas)
+        this.alas = alas
+    }
+    ponedora(){
+        return true
+    }
+    comer2(){
+        return `${this.nombre} come mucho maiz`
+    }
+};
 
 const Perrito1 = new Perrito('Rocko', 4, 'cafe');
 
 console.log(Perrito1)
 console.log(Perrito1.comer())
 console.log(Perrito1.ladrar())
+
+const Gato1 = new Gato('Michi', 4, 'angora');
+
+console.log(Gato1)
+console.log(Gato1.comer())
+console.log(Gato1.maulla())
+
+const Hamters1 = new Hamters('Pufi', 4, 'dorado');
+
+console.log(Hamters1)
+console.log(Hamters1.comer())
+console.log(Hamters1.jugar())
+
+const Gallina1 = new Gallina('Beti', 2, 2);
+
+console.log(Gallina1)
+console.log(Gallina1.comer2())
+console.log(Gallina1.ponedora())
