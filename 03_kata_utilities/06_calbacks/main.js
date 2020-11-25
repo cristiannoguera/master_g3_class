9ec31a2f3c5e99acb329c2d36f4b4f956e18d39e
cristiannoguera <cristian.noguera@clouxter.com>
2020-11-24 20:58:15 -0500

    callbacks

---
+++ 03_kata_utilities/06_calbacks/main.js
@@ -0,0 +1,10 @@
+const timer = (callback, time) => {
+    for (let i = 0; i >time; i++) {
+        if (i === time) {
+            callback
+        }
+    }
+}
+setTimeout(function () {
+    console.log('Hola a todos')
+} , 1000)
