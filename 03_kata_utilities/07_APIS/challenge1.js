ee9ba0204f90d5c81a8598ee0ec2e730b3f30c3c
cristiannoguera <cristian.noguera@clouxter.com>
2020-12-01 22:42:37 -0500

    challenge

--- 03_kata_utilities/07_APIS/challenge1.js
+++ 03_kata_utilities/07_APIS/challenge1.js
@@ -3,18 +3,18 @@
 // Buscar un libro y traer el o los autores del primer libro
 // http://openlibrary.org/search.json?q=i+robot)
 
+
 const request = require('request');
-
-const buscarLibro = (libro) => {
-
-    request.get(`http://openlibrary.org/${libro}`, (error, response, body) => {
+const buscarLibro = () => {
+    request.get('http://openlibrary.org/search.json?q=i+robot', (error, response, body) => {
         if (response.statusCode === 200) {
-            const data = JSON.parse(body)
-            console.log(data);
-            console.log(`${data.tittle}`)
+            const data = JSON.parse(body);
+            //console.log(JSON.parse(body));
+            console.log(`El Autor se llama: ${data.docs[0].author_name}`);
+            console.log(`Fecha de publicacion: ${data.docs[0].publish_date}`);
         }else {
-            console.error(`'Ocurrio un error en la peticion ${error}`);
+            console.log(`'Ocurrio un error en la peticion ${error}`)
     }
 });
 }
-buscarLibro('i robot')
+buscarLibro()
